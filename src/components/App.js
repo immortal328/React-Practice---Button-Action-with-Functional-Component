1b9c49b2bdc804788df8b8ddcf9c3a48c17dec03
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-12 21:26:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import "./../styles/App.css";
 
 function App(){
   
-  const [text,setText] =useState("");
+  const [text,setText] =useState(false);
   const txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
   
   return (
     <div id="main">
       // Do not alter the main div
-      <button id="click"onClick={function(){setText(txt)}}>Toggle</button>
-      <p id="para">{text}</p>
+      <button id="click" onClick={function(){setText(!text)}}>Toggle</button>
+      {text? (<p id="para">{txt}</p>):null}
     </div>
   );
 }
