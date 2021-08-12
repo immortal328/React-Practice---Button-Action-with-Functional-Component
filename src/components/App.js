83f3132c80d5ba72ba6672ff99867ede179a3218
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-12 20:45:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function App() {
+function App(){
+  
+  const [text,setText] =useState("");
+  const txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click"onClick={function(){setText(txt)}}>Toggle</button>
+      <p id="para">{text}</p>
     </div>
   );
 }
